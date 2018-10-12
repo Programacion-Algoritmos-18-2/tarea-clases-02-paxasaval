4d8283b336a8b69770d0a25ec6e539d876c85a0f
Paul Sanchez <paxasaval1003@gmail.com>
2018-10-12 10:23:44 -0500

    Subiendo v1.0

---
+++ Tarea-clases-02-paxasaval/src/tarea/clases/pkg02/paxasaval/Precio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.clases.pkg02.paxasaval;
+
+/**
+ *
+ * @author User
+ */
+public class Precio {
+    double euros;
+    public double obtener (){
+        return 0;
+        //tooo
+    }
+    public void agregar (double x){
+        
+    }
+       
+}
